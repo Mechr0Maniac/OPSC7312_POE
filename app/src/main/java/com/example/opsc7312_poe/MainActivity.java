eb70b947caa0b9b2626da73a366d87e296eb0b0a
ChloeCantCode <56159061+Chloecantplay@users.noreply.github.com>
2021-11-11 18:18:13 +0200

    Added Firebase Login +Linked to Firebase

--- app/src/main/java/com/example/opsc7312_poe/MainActivity.java
+++ app/src/main/java/com/example/opsc7312_poe/MainActivity.java
@@ -1,53 +1,105 @@
 package com.example.opsc7312_poe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    private Button btnNewRegister,btnEnter;
+    private EditText edtPass,edtUser;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnNewRegister =(Button) findViewById(R.id.btnNewRegister);
+        btnNewRegister.setOnClickListener(this);
 
-        Button addCat = findViewById(R.id.btnEnter);
-        Button newRegister = findViewById(R.id.btnNewRegister);
-        EditText user = findViewById(R.id.edtUser);
-        EditText pass = findViewById(R.id.edtPass);
+        btnEnter=(Button) findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(this);
 
-        addCat.setOnClickListener(new View.OnClickListener() {
+        edtUser=(EditText) findViewById(R.id.edtUser);
+        edtPass=(EditText) findViewById(R.id.edtPass);
+
+        progressBar=(ProgressBar)  findViewById(R.id.progressBar);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnNewRegister:
+                startActivity(new Intent(this,UserRegister.class));
+                break;
+
+            case R.id.btnEnter:
+                userLogin();
+                break;
+        }
+    }
+
+    private void userLogin() {
+        String email =edtUser.getText().toString().trim();
+        String password=edtPass.getText().toString().trim();
+
+        if(email.isEmpty()){
+            edtUser.setError("Email is required");
+            edtUser.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edtUser.setError("Please enter a valid email");
+            edtUser.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            edtPass.setError("Password is required");
+            edtPass.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            edtPass.setError("Min password length is 6 characters");
+            edtPass.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View v) {
-                String sUser = user.getText().toString();
-                String sPass = pass.getText().toString();
-
-                if (sUser.equals("User") && sPass.equals("12345")){
-                    Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                    startActivity(intent);
-                } else if (user.equals("") && pass.equals("")){
-                    Toast.makeText(MainActivity.this, "Please enter your username and password", Toast.LENGTH_SHORT).show();
-                } else if (user.equals("")){
-                    Toast.makeText(MainActivity.this, "Please enter your username", Toast.LENGTH_SHORT).show();
-                } else if (pass.equals("")) {
-                    Toast.makeText(MainActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Username or password not valid", Toast.LENGTH_SHORT).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //redirect to Map Activity
+                    startActivity(new Intent(MainActivity.this,MapActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this, "Failed to Log in, recheck credentials", Toast.LENGTH_LONG).show();
                 }
             }
         });
-        newRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent reg = new Intent(MainActivity.this, UserRegister.class);
-                startActivity(reg);
-            }
-        });
+
     }
 }
